f463f31acea15a931b517502137e07189e556a4a
lytrunghieu <hieult@nexlesoft.com>
2024-11-12 22:11:42 +0700

    update script run android test

--- scripts/run_test_android.sh
+++ scripts/run_test_android.sh
@@ -1,9 +1,16 @@
-# kill proxy process on exit
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Kill proxy process on exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+# Start Emulator
 nohup emulator -avd Pixel_4_XL_API_30 -dns-server "8.8.8.8,8.8.4.4" -no-snapshot -no-boot-anim &
 
-cd ./mobileapp
+
+cd $SCRIPT_DIR
+cd ../
+cd "./mobileapp" || exit
 yarn build_android_test
 yarn test_android
 # kill the Android Mobile emulator
