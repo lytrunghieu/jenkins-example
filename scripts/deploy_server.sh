5cc1fcf71dbf7a53254fa90800ae8bc50ed3bb4a
lytrunghieu <hieult@nexlesoft.com>
2024-11-21 22:08:58 +0700

    continue

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -5,7 +5,7 @@ HEROKU_API_KEY=$2
 
 echo $HEROKU_API_KEY | heroku auth:token
 heroku config:set HEROKU_APP_DIRECTORY=server --app $HEROKU_APP_NAME
-git push --force
+git push --force heroku main
 
 # git subtree pull --prefix=server https://git.heroku.com/$HEROKU_APP_NAME.git main --squash
 # git checkout main
